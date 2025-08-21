ad4ee317251430804d5e7b924b823e7dfb0cc13f
Vinodini <bvinodini04@gmail.com>
2025-08-21 23:38:42 +0530

    Fixed HuggingFaceEmbeddings import and updated requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import HuggingFacePipeline
